c650dbc217381c6b9cdffd7536142d7e2693ab26
omaralswaitti <116668908+omaralswaitti@users.noreply.github.com>
2022-10-25 13:52:25 -0400

    Create triangle.c

---
+++ triangle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()  
+{  
+    int num, i, j, m = 1;
+    printf (" Enter the triangle height: ");  
+    scanf ("%d", & num);  
+    for (i = 1; i <= num; i++)  
+    {  
+        for (j = 1; j <= i; j++)  
+        {  
+            printf( "! ");  
+        }  
+        printf("\n");  
+    }  
+    for (i = num-1; i >= 1; i--)  
+    {  
+        for (j = 1; j <= i; j++)  
+        {  
+            printf ("! ");  
+        }  
+        printf("\n");  
+      }
+  }
